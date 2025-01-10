3f7909af397b293e224d9e576bf9f910c321d1c5
KoraOrvatnew <mironovich.alena83@gmail.com>
2025-01-10 21:17:02 +0300

    js

---
+++ lesson11/index.js
@@ -0,0 +1,16 @@
+// let a = 1000;
+// console.log(b);
+// // b = 500;
+// console.log(a);
+// const a = 1000;
+// console.log(a);
+// // a = 500;
+
+// const c1 = 43;
+// const str =' cmcmc"мльлвьсл"vvvv' выделение кавычками
+// const str = `1+1`=${1+1} в консоли виден результат
+// alert ("сообщение в браузере");
+// consol.log ("сообщение")
+// confirm ("введите...")
+const nickName = prompt ("введите ваше имя", "Алена");
+console.log("Привет, ${nickName}");

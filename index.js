f7265fe7d26459f7f9de35053ac6a6044af8ea20
KoraOrvatnew <mironovich.alena83@gmail.com>
2025-01-14 21:23:25 +0300

    lesson11_12

---
+++ index.js
@@ -0,0 +1,62 @@
+// const arr = [1, "two", null, true, { a: 2 }, [1, 2, 3]];
+// // console.obj = arr[4];
+// // console.log(obj);
+// console.log(arr[5][1]);
+// 
+
+// const arr = [0, 1, 2, 3, 4];
+// const arr2 = [5, 6, 7, 8, 9];
+
+// console.log(arr.concat(arr2));
+// arr.push("five","seven","eight");
+// console.log(arr)
+// arr.unshift("five","seven","eight");
+// console.log(arr);
+
+// const data = arr.pop();
+// console.log(arr);
+// console.log(data);
+
+// const data = arr.shift();
+// // console.log(arr);
+// // console.log(data);
+
+// console.log(arr.reverse);
+// console.log(arr);
+
+// const arrSlice = arr.slice (1, 4)
+// console.log(arr.slice);
+
+// const arrSplice = arr.splice (1, 3, 11, 22, 33);
+// console.log("arr", arr);
+
+// const age = 18;
+// if (age > 18) {
+//     console.log("> 18");
+    
+// } else if (age < 18){
+//     console.log("< 18");
+    
+// } else {
+//     console.log("= 18");
+    
+// }
+
+// const age = 18;
+// if (age < 18) {
+//     console.log("< 18");
+    
+// } else if (age < 19) {
+//     console.log("< 19");
+    
+// } else if (age < 20) {
+//     console.log("< 20");
+// } else if (age < 21) {
+//     console.log("< 21");
+// }
+
+
+console.log((93125 % 8));
+
+
+
--- lesson11/index.js
+++ lesson11/index.js
@@ -1,16 +1,52 @@
-// let a = 1000;
-// console.log(b);
-// // b = 500;
-// console.log(a);
-// const a = 1000;
-// console.log(a);
-// // a = 500;
+// // let a = 1000;
+// // console.log(b);
+// // // b = 500;
+// // console.log(a);
+// // const a = 1000;
+// // console.log(a);
+// // // a = 500;
 
-// const c1 = 43;
-// const str =' cmcmc"мльлвьсл"vvvv' выделение кавычками
-// const str = `1+1`=${1+1} в консоли виден результат
-// alert ("сообщение в браузере");
-// consol.log ("сообщение")
-// confirm ("введите...")
-const nickName = prompt ("введите ваше имя", "Алена");
-console.log("Привет, ${nickName}");
+// // const c1 = 43;
+// // const str =' cmcmc"мльлвьсл"vvvv' выделение кавычками
+// // const str = `1+1`=${1+1} в консоли виден результат
+// // alert ("сообщение в браузере");
+// // consol.log ("сообщение")
+// // confirm ("введите...")
+// const nickName = prompt ("введите ваше имя", "Алена");
+// console.log("Привет, ${nickName}");
+
+// const str = "SOME random STrinG"
+
+// console.log (str.toUpperCase());
+// console.log (str.toLowerCase());
+
+
+// const str2 = "next string"
+// console.log(str.concat (str2, 2254))
+// console.log ("50" + 25);
+// console.log ("50" - 25);
+
+// console.log(str.repeat(5));
+
+// console.log(str.toLowerCase().includes("str"));
+
+
+// console.log(str.startsWith("SOME"));
+
+// console.log(str.endsWith("some"));
+
+
+// console.log(str.replace("S", "$"));
+// console.log(str.replaceAll("S", "$"));
+// console.log(str.replaceAll(/S/gi, "$"));
+
+const str = "SOME random STrinG";
+
+// console.log (str.charAt(6)); индексы считаются с 0: 012345...
+// console.log (str.indexOf("random"));
+// console.log (str.lastIndexOf("S"));
+
+// console.log (str.slice(5, 11));
+// console.log (str.substring(5, 11));
+// console.log (str.substr(5, 6)); метод не дорабатывается 5 индекс откуда считаем, 6 - количество символов к отображению  (подчету)
+// console.log(str.trim() обрезает пробелы в начале и конце строки
